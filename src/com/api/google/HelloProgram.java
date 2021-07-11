4c5514d47f227d9e2fe636506bcf67fe5a8bc425
Surjeet Mohanty <surjeetmohanty26@gmail.com>
2021-07-11 22:05:43 +0530

    Method added

--- src/com/api/google/HelloProgram.java
+++ src/com/api/google/HelloProgram.java
@@ -2,9 +2,14 @@ package com.api.google;
 
 public class HelloProgram {
 
+	public static void m(String str) {
+		System.out.println("Get Say"+ str);
+	}
 	public static void main(String[] args) {
 
-		
+		for(int i=0;i<10;i++) {
+			m("Hey!"+ i);
+		}
 	}
 
 }
