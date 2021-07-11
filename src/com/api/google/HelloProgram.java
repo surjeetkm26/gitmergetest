e05222f10592ec40f6c02f812c68f021c082d5f1
surjeetkm26 <69843349+surjeetkm26@users.noreply.github.com>
2021-07-11 22:11:37 +0530

    Update HelloProgram.java

--- src/com/api/google/HelloProgram.java
+++ src/com/api/google/HelloProgram.java
@@ -1,7 +1,9 @@
 package com.api.google;
-
+import java.util.*
 public class HelloProgram {
-
+	public static void sortAnArray(){
+		System.out.println("Merge Sort");
+	}
 	public static void main(String[] args) {
 
 		System.out.println("Good Morning");
